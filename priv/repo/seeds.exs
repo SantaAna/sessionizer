d3f1b378fa790ade9a8f12c38182d19b8160afa6
Patrick <patrick@pop-os.localdomain>
2023-08-23 00:21:52 -0700

    added basic seeding

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+for first_name <- ~w(bob sally jill), last_name <- ~w(jones smith black), cohort_number <- [1,2,3] do
+  %{first_name: first_name, last_name: last_name, cohort_number: cohort_number}
+end
+|> Enum.each(&Sessionizer.Students.new_from_form/1)
