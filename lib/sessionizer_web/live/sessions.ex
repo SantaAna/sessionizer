53ff7104fc53f6048c91852dd7405f643b4c5a37
Patrick <patrick@pop-os.localdomain>
2023-08-26 17:07:54 -0700

    added basic session tracking

--- lib/sessionizer_web/live/sessions.ex
+++ lib/sessionizer_web/live/sessions.ex
@@ -4,24 +4,67 @@ defmodule SessionizerWeb.Sessions do
   alias Sessionizer.Cohorts
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, cohort_numbers: Cohorts.all() |> Enum.map(& &1.cohort_number))}
+    {:ok,
+     assign(socket,
+       cohort_numbers: Cohorts.all() |> Enum.map(& &1.cohort_number),
+       driver: nil,
+       navigator: nil,
+       start_time: nil,
+       session_running: false
+     )}
   end
 
   def render(assigns) do
-  ~H"""
-    <h1> Welcome to Sessions! </h1> 
-    <div >
+    ~H"""
+    <h1 class="text-2xl mb-5">Welcome to Sessions!</h1>
+    <div class="flex flex-row gap-10 justify-center">
       <label for="cohort-select">Choose a Cohort</label>
       <select name="cohort-select" phx-hook="CohortSelection" id="cohort-select">
-        <option :for={cohort_number <- @cohort_numbers} value={cohort_number}> <%= cohort_number %> </option> 
+        <option :for={cohort_number <- @cohort_numbers} value={cohort_number}>
+          <%= cohort_number %>
+        </option>
       </select>
+      <.button phx-click="next-pair">
+        Next Pair
+      </.button>
     </div>
-  """ 
+    <div :if={@session_running} class="flex flex-row gap-3">
+      <p class="text-lg">Navigator: <%= @navigator.first_name %></p>
+      <p class="text-lg">Driver: <%= @driver.first_name %></p>
+      <.button phx-click="stop-session">
+        Stop Sessoin
+      </.button>
+    </div>
+    """
   end
 
-  def handle_event("cohort-selected", unsigned_params, socket) do
-    IO.puts("fired event")
-    IO.inspect(unsigned_params) 
-    {:noreply, socket}
+  def handle_event("stop-session", _unsigned_params, socket) do
+    IO.puts("called stop")
+
+    {:noreply,
+     assign(socket,
+       driver: nil,
+       navigator: nil,
+       start_time: nil,
+       session_running: false
+     )}
+  end
+
+  def handle_event("next-pair", _unsigned_params, socket) do
+    {:ok, [navigator, driver]} = CohortCache.next_pair(socket.assigns.cohort_number)
+
+    IO.inspect([driver, navigator])
+
+    {:noreply,
+     assign(socket,
+       driver: driver,
+       navigator: navigator,
+       start_time: NaiveDateTime.utc_now(),
+       session_running: true
+     )}
+  end
+
+  def handle_event("cohort-selected", %{"cohort_number" => cohort_number}, socket) do
+    {:noreply, assign(socket, cohort_number: String.to_integer(cohort_number))}
   end
 end
