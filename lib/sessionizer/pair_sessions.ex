f2c4888e0a695cb157faf0c78a291ca7a5ecda4d
Patrick <patrick@pop-os.localdomain>
2023-08-26 17:40:23 -0700

    basic session tracking in db

---
+++ lib/sessionizer/pair_sessions.ex
@@ -0,0 +1,23 @@
+defmodule Sessionizer.PairSessions do
+  import Ecto.Query 
+  alias Sessionizer.PairSessions.PairSession
+  alias Sessionizer.Repo
+
+  def create(attrs \\ %{})
+
+  def create(%{driver: driver, navigator: navigator} = attrs) when is_map(driver) and is_map(navigator) do
+    attrs = attrs 
+    |> Map.put(:driver_id, driver.id) 
+    |> Map.put(:navigator_id, navigator.id) 
+
+    %PairSession{}
+    |> PairSession.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def create(attrs) do
+    %PairSession{}
+    |> PairSession.changeset(attrs)
+    |> Repo.insert()
+  end
+end
--- lib/sessionizer_web/live/sessions.ex
+++ lib/sessionizer_web/live/sessions.ex
@@ -2,15 +2,21 @@ defmodule SessionizerWeb.Sessions do
   use SessionizerWeb, :live_view
   alias Sessionizer.CohortCache
   alias Sessionizer.Cohorts
+  alias Sessionizer.PairSessions
 
   def mount(_params, _session, socket) do
+    cohort_numbers = Cohorts.all() |> Enum.map(& &1.cohort_number)
+    cohort_number = hd(cohort_numbers)
+
     {:ok,
      assign(socket,
-       cohort_numbers: Cohorts.all() |> Enum.map(& &1.cohort_number),
+       cohort_numbers: cohort_numbers,
+       cohort_number: cohort_number,
        driver: nil,
        navigator: nil,
        start_time: nil,
-       session_running: false
+       session_running: false,
+       session_count: 0
      )}
   end
 
@@ -24,6 +30,7 @@ defmodule SessionizerWeb.Sessions do
           <%= cohort_number %>
         </option>
       </select>
+      <p><%= @session_count %></p>
       <.button phx-click="next-pair">
         Next Pair
       </.button>
@@ -31,6 +38,7 @@ defmodule SessionizerWeb.Sessions do
     <div :if={@session_running} class="flex flex-row gap-3">
       <p class="text-lg">Navigator: <%= @navigator.first_name %></p>
       <p class="text-lg">Driver: <%= @driver.first_name %></p>
+      <p class="text-lg">Timer: <%= @timer %></p>
       <.button phx-click="stop-session">
         Stop Sessoin
       </.button>
@@ -41,12 +49,20 @@ defmodule SessionizerWeb.Sessions do
   def handle_event("stop-session", _unsigned_params, socket) do
     IO.puts("called stop")
 
+    PairSessions.create(%{
+      driver: socket.assigns.navigator,
+      navigator: socket.assigns.driver,
+      start_time: socket.assigns.start_time, end_time: NaiveDateTime.utc_now()
+    })
+
     {:noreply,
      assign(socket,
        driver: nil,
        navigator: nil,
        start_time: nil,
-       session_running: false
+       session_running: false,
+       session_count: socket.assigns.session_count + 1,
+       timer: 0
      )}
   end
 
@@ -54,17 +70,28 @@ defmodule SessionizerWeb.Sessions do
     {:ok, [navigator, driver]} = CohortCache.next_pair(socket.assigns.cohort_number)
 
     IO.inspect([driver, navigator])
+    Process.send_after(self(), {:tick, socket.assigns.session_count}, 1000)
 
     {:noreply,
      assign(socket,
        driver: driver,
        navigator: navigator,
        start_time: NaiveDateTime.utc_now(),
-       session_running: true
+       session_running: true,
+       timer: 0
      )}
   end
 
   def handle_event("cohort-selected", %{"cohort_number" => cohort_number}, socket) do
     {:noreply, assign(socket, cohort_number: String.to_integer(cohort_number))}
+  end
+
+  def handle_info({:tick, count}, %{assigns: %{session_count: count}} = socket) do
+    Process.send_after(self(), {:tick, count}, 1000)
+    {:noreply, update(socket, :timer, &(&1 + 1))}
+  end
+
+  def handle_info({:tick, _count}, socket) do
+    {:noreply, socket}
   end
 end
